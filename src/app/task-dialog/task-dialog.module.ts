f99047960728e07d05bd5933b45f16108b9947b2
MarcoSorci <97511096+MarcoSorci@users.noreply.github.com>
2022-03-25 11:41:12 +0100

    creation

---
+++ src/app/task-dialog/task-dialog.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TaskDialogComponent } from './component/task-dialog.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    TaskDialogComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TaskDialogModule { }
